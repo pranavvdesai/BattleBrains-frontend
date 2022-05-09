1e45ec87ff0d5ced7f7ef89afafac7d9e8b44563
amandesai01 <amandesai01@gmail.com>
2022-05-10 00:31:08 +0530

    nuxtv2

---
+++ tailwind.config.js
@@ -0,0 +1,68 @@
+module.exports = {
+    content: [
+      "app.vue",
+      "./components/**/*.{js,vue,ts}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./plugins/**/*.{js,ts}",
+    ],
+    theme: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      colors: {
+        transparent: 'transparent',
+  
+        white: '#FFFFFF',
+        black: '#000000',
+  
+        'light': {
+          100: '#E6E8F2',
+          200: '#B8BBCC',
+          300: '#4C4C66',
+          400: '#262633',
+        },
+  
+        'dark': {
+          100: '#262633',
+          200: '#14141A',
+          300: '#3A3A4D',
+          400: '#9898B3',
+          500: '#CFCFE6',
+        },
+  
+        'lock-cover': {
+          'light': '#262633',
+          'dark': '#12121A'
+        },
+  
+        'blue': {
+          'normal': '#5B95E6',
+          'sky': '#5CB8E6'
+        },
+  
+        'pink': '#E65BE6',
+        'green': '#5BE672',
+        'yellow': '#E6E65B',
+      },
+      fontFamily: {
+        mont: ['Montserrat', 'sans-serif'],
+        josefin: ['Josefin Sans', 'sans-serif'],
+      },
+      extend: {
+        spacing: {
+          '128': '32rem',
+          '144': '36rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        }
+      }
+    },
+    plugins: [],
+  }
+  
