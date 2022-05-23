213cf4fc4e3c1ce52c437c8befdb67b9655075f8
pranavvdesai <pranavdesai.psd@gmail.com>
2022-05-23 15:32:16 +0530

    fix: tailwind postcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,68 +1,72 @@
 module.exports = {
-    content: [
-      "app.vue",
-      "./components/**/*.{js,vue,ts}",
-      "./layouts/**/*.vue",
-      "./pages/**/*.vue",
-      "./plugins/**/*.{js,ts}",
-    ],
-    theme: {
+  mode: "jit",
+  content: [
+    "index.vue",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./store/**/*.{js,ts}",
+    "./static/**/*.{js,ts}",
+    "./nuxt.config.js",
+  ],
+  theme: {
+    extend: {
       screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
       colors: {
-        transparent: 'transparent',
-  
-        white: '#FFFFFF',
-        black: '#000000',
-  
-        'light': {
-          100: '#E6E8F2',
-          200: '#B8BBCC',
-          300: '#4C4C66',
-          400: '#262633',
+        transparent: "transparent",
+
+        white: "#FFFFFF",
+        black: "#000000",
+
+        light: {
+          100: "#E6E8F2",
+          200: "#B8BBCC",
+          300: "#4C4C66",
+          400: "#262633",
         },
-  
-        'dark': {
-          100: '#262633',
-          200: '#14141A',
-          300: '#3A3A4D',
-          400: '#9898B3',
-          500: '#CFCFE6',
+
+        dark: {
+          100: "#262633",
+          200: "#14141A",
+          300: "#3A3A4D",
+          400: "#9898B3",
+          500: "#CFCFE6",
         },
-  
-        'lock-cover': {
-          'light': '#262633',
-          'dark': '#12121A'
+
+        "lock-cover": {
+          light: "#262633",
+          dark: "#12121A",
         },
-  
-        'blue': {
-          'normal': '#5B95E6',
-          'sky': '#5CB8E6'
+
+        blue: {
+          normal: "#5B95E6",
+          sky: "#5CB8E6",
         },
-  
-        'pink': '#E65BE6',
-        'green': '#5BE672',
-        'yellow': '#E6E65B',
+
+        pink: "#E65BE6",
+        green: "#5BE672",
+        yellow: "#E6E65B",
       },
       fontFamily: {
-        mont: ['Montserrat', 'sans-serif'],
-        josefin: ['Josefin Sans', 'sans-serif'],
+        mont: ["Montserrat", "sans-serif"],
+        josefin: ["Josefin Sans", "sans-serif"],
       },
-      extend: {
-        spacing: {
-          '128': '32rem',
-          '144': '36rem',
-        },
-        borderRadius: {
-          '4xl': '2rem',
-        }
-      }
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+        200: "53.125rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
