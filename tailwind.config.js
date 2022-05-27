544e2c77517eec539bde8695ff8bc9118ce4d7c1
pranavvdesai <pranavdesai.psd@gmail.com>
2022-05-27 13:27:56 +0530

    fix: theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,55 +11,55 @@ module.exports = {
     "./nuxt.config.js",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    colors: {
+      transparent: "transparent",
+
+      white: "#FFFFFF",
+      black: "#000000",
+      gray: "#B8B8CC",
+      light: {
+        100: "#E6E8F2",
+        200: "#B8BBCC",
+        300: "#4C4C66",
+        400: "#262633",
+      },
+
+      dark: {
+        100: "#262633",
+        200: "#14141A",
+        300: "#3A3A4D",
+        400: "#9898B3",
+        500: "#CFCFE6",
+        600: "#1C1C26",
+      },
+
+      "lock-cover": {
+        light: "#262633",
+        dark: "#12121A",
+      },
+
+      blue: {
+        normal: "#5B95E6",
+        sky: "#5CB8E6",
+        border: "#3B8DFF",
+      },
+
+      pink: "#E65BE6",
+      green: "#5BE672",
+      yellow: "#E6E65B",
+    },
+    fontFamily: {
+      mont: ["Montserrat", "sans-serif"],
+      josefin: ["Josefin Sans", "sans-serif"],
+    },
     extend: {
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
-      },
-      colors: {
-        transparent: "transparent",
-
-        white: "#FFFFFF",
-        black: "#000000",
-        gray: "#B8B8CC",
-        light: {
-          100: "#E6E8F2",
-          200: "#B8BBCC",
-          300: "#4C4C66",
-          400: "#262633",
-        },
-
-        dark: {
-          100: "#262633",
-          200: "#14141A",
-          300: "#3A3A4D",
-          400: "#9898B3",
-          500: "#CFCFE6",
-          600: "#1C1C26",
-        },
-
-        "lock-cover": {
-          light: "#262633",
-          dark: "#12121A",
-        },
-
-        blue: {
-          normal: "#5B95E6",
-          sky: "#5CB8E6",
-          border: "#3B8DFF",
-        },
-
-        pink: "#E65BE6",
-        green: "#5BE672",
-        yellow: "#E6E65B",
-      },
-      fontFamily: {
-        mont: ["Montserrat", "sans-serif"],
-        josefin: ["Josefin Sans", "sans-serif"],
-      },
       spacing: {
         128: "32rem",
         144: "36rem",
