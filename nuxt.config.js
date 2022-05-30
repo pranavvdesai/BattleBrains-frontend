6f2b80cafdf03a914885f9e215fc260ef7ec7418
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2022-05-30 10:37:38 +0530

    1. npm run dev error solved. 2. new color palette added. 3. lock files excluded from push.

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~/assets/css/main.css"],
+  css: ["@/assets/css/main.css"],
+
+  devServerHandlers: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,37 @@
 <template>
-  <section class="h-screen bg-dark-100">
-    <div class="flex w-full justify-start p-5">
-      <div class="w-1/6 px-5 py-2">
-        <img
-          src="@/assets/images/logo-dark_mode.svg"
-          alt="Battlebrains Logo"
-        >
-      </div>
-    </div>
-    <div class="flex w-full">
-      <div class="w-full md:w-1/2 sm:pb-0 p-20 md:p-32">
-        <img
-          src="@/assets/images/khaby.png"
-          alt="Khaby Image"
-        >
-      </div>
-      <div class="w-1/2 m-8 p-32 -translate-x-8 rounded-3xl bg-dark-200">
-        <img
-          src="@/assets/images/wagmi.svg"
-          alt="We All Gonna Make It!"
-          class="w-1/2 h-auto mx-auto justify-self-center self-center"
-        >
-        <p class="text-center mt-5 mb-10 w-full text-dark-500 font-mont text-sm justify-self-center self-center">Maximising the creative idea in Education to defy gravity and matter in Web3.</p>
-        <button class="bb-button flex items-center">
-          <img
-            src="@/assets/images/metamask-logo.svg"
-            class="h-8 w-8 mr-2"
-          /> <span>Connect using Metamask</span>
-        </button>
+  <section class="page">
+    <div class="page_div">
 
-        <div
-          class='w-1/2 border-b border-dark-400 text-center justify-self-center mx-auto'
-          style="height: 20px"
-        >
-          <span class='bg-dark-200 px-5 text-dark-400 font-mont -translate-y-16'>
-            or
-          </span>
+      <div class="khaby_div">
+        <div class="py-10 justify-center flex lg:justify-between">
+          <img src="@/assets/images/logo-dark_mode.svg" class="w-1/2 h-auto lg:w-1/3" alt="Khaby Image"/>
         </div>
+        <img src="@/assets/images/khaby.png" class="khaby_image"/>
+      </div>
 
-        <input
-          type="email"
-          class="bb-input placeholder:text-center"
-          placeholder="Email"
-        />
-        <button class="bb-button-blue hover:bg-green-700">
-          Sign in with Email
-        </button>
+      <div class="flex-1 flex justify-center mb-10 lg:my-10">
+        <div class="login">
+          <img src="@/assets/images/wagmi.svg" alt="wagmi" class="wagmi">
+          <p class="text">Maximising the creative idea in Education to defy gravity and matter in Web3.</p>
+          <button class="metamask_login">
+            Connect using Metamask
+          </button>
+
+          <div class='w-1/2 border-b border-dark-400 text-center justify-self-center mx-auto' style="height: 20px">
+              <span class='bg-dark-200 px-5 text-dark-400 font-mont'>
+                  or
+              </span>
+          </div>
+
+          <input
+            type="email"
+            class="email_input"
+            placeholder="Enter email"
+          />
+          <button class="email_login hover:bg-green-700">
+            Sign in with Email
+          </button>
+        </div>
       </div>
     </div>
   </section>
