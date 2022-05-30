293553e8aa273d7df0570381d5e43ddf88f6b7ce
amandesai01 <amandesai01@gmail.com>
2022-05-30 13:01:15 +0530

    component auto import off

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: false,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: ["@nuxt/postcss8", "@nuxtjs/tailwindcss"],
