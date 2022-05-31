c7096864e20e639a1449b7907b251b7df802e41a
krishvadhani19 <krish.vadhani@somaiya.edu>
2022-06-01 01:22:50 +0530

    error with image opacity

---
+++ pages/home.vue
@@ -0,0 +1,76 @@
+<template>
+  <body class="bg-dark-100">
+    <div class="flex h-64 sm:h-72 md:h-80 lg:h-96 px-5 -z-10">
+      <div
+        class="rightSideBar bg-dark-200 rounded-lg w-full flex justify-between flex-col md:flex-row lg:flex-row"
+      >
+        <!-- 1 -->
+        <div
+          class="flex flex-col justify-between p-5 w-full h-1/2 lg:w-1/2 lg:h-full"
+        >
+          <div class="z-50">
+            <div class="flex text-sm font-medium">
+              <p class="mr-6 text-[#01E3FF]">INTRODUCTION</p>
+              <p class="mr-6 text-[#FF01EC]">WEB3</p>
+              <p class="mr-6 text-[#3AFF01]">NFTS</p>
+            </div>
+            <div
+              class="font-black text-white text-3xl sm:text-5xl md:text-6xl lg:text-7xl my-1"
+            >
+              <p>Why Web3?</p>
+            </div>
+            <div class="text-[#9CA3AF] font-medium lg:w-2/3 text-lg">
+              The third generation of the web promises to put us in control of
+              our data...
+            </div>
+          </div>
+          <div
+            class="flex text-white lg:justify-between flex-col lg:flex-row z-50"
+          >
+            <div class="flex mt-2">
+              <p class="mr-2">3/4 Videos</p>
+              <div class="bg-white w-24 h-3 items-center rounded-lg mt-2 -z-50">
+                <div
+                  class="bg-gradient-to-r from-[#00BCD4] to-[#1e90ff] ... w-3/4 h-3 items-center rounded-lg z-10"
+                ></div>
+              </div>
+            </div>
+            <div class="flex items-center mt-2">
+              <img
+                class="mr-2 w-4 h-4"
+                src="../assets/images/boldtime.png"
+                alt=""
+              />
+              <p class="">66mins</p>
+            </div>
+            <div class="flex items-center mt-2">
+              <img
+                class="mr-2 w-4 h-4"
+                src="../assets/images/paper.png"
+                alt=""
+              />
+              <p class="">5 references</p>
+            </div>
+          </div>
+        </div>
+        <!-- 2 -->
+        <div class="w-full h-1/2 lg:w-1/2 lg:h-full">
+          <div
+            class="bg-dark-200 bg-gradient-to-b lg:bg-gradient-to-r from-opacity-100 to-opacity-0 z-20"
+          >
+            <div
+              class="text-white border-4 rounded-md font-medium border-[#0b0ed8] px-5 pb-1 z-30 inline-block"
+            >
+              Beginners
+            </div>
+            <img
+              class="z-0 bg-no-repeat bg-center bg-cover w-full h-full"
+              src="../assets/images/card1.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</template>
