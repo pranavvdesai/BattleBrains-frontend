ded4ff5053d2a3de563395a46c2b506311809b3a
krishvadhani19 <krish.vadhani@somaiya.edu>
2022-06-02 00:11:43 +0530

    icon change 2 ; sidebar and togglebar pending

--- components/modal.vue
+++ components/modal.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="px-4 pb-4">
-
     <div class="flex mt-5">
       <h2 class="text-blue-normal mr-2 text-sm font-josefin">INTRODUCTION</h2>
       <h2 class="text-pink text-sm font-josefin">BLOCKCHAIN</h2>
     </div>
-    <p class="mt-1 text-base text-gray font-josefin">A blockchain is a digitally distributed, decentralized, public ledger that exists across a network...</p>
+    <p class="mt-1 text-base text-gray font-josefin">
+      A blockchain is a digitally distributed, decentralized, public ledger that
+      exists across a network...
+    </p>
     <div class="flex items-center mt-2">
       <h1 class="text-white mr-3">2/4 videos</h1>
       <div class="w-1/2 bg-white rounded-lg">
@@ -13,32 +15,30 @@
       </div>
     </div>
     <div class="flex items-center my-2">
-      <img
-        src="@/assets/images/boldtime.png"
-        alt="Paper"
-        class=" mr-2 "
-      >
+      <BoldTimeCircleIcon :size="20" :color="'#fff'" class="mr-2" />
       <p class="text-white font-josefin">66 mins</p>
     </div>
     <div class="flex items-center my-2">
-      <img
-        src="@/assets/images/boldpaper.png"
-        alt="Paper"
-        class="w-3 h-4 mr-2 "
-      >
+      <BoldPaperIcon :size="20" :color="'#fff'" class="mr-2" />
       <p class="text-white font-josefin">5 references</p>
     </div>
-    <button class="text-white font-josefin border-2 border-blue-border px-3 py-1 rounded-md mt-1 ">Begginers</button>
+    <button
+      class="text-white font-josefin border-2 border-blue-border px-3 py-1 rounded-md mt-1"
+    >
+      Begginers
+    </button>
   </div>
-
 </template>
 
 <script>
+import BoldPaperIcon from "./icons/boldPaperIcon.vue";
+import BoldTimeCircleIcon from "./icons/boldTimeCircleIcon.vue";
 export default {
   methods: {
     toggle() {
       this.$emit("toggle");
     },
   },
+  components: { BoldPaperIcon, BoldTimeCircleIcon },
 };
 </script>
