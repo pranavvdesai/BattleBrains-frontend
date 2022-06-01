9ca5bf3242d1946d3995c389a4edd86ada418e47
DEV270201 <devansh.js@somaiya.edu>
2022-06-01 14:53:48 +0530

    modified files

---
+++ components/Bottombar.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- Mobile View -->
+  <div class="bottombar justify-around items-center bg-dark-100 fixed bottom-0">
+      <NuxtLink to="/home" class="flex p-2">
+             <img src="@/assets/images/Iconly-Bold-Home.svg" class="img" height="15px" width="15px"/>
+          </NuxtLink>
+            <NuxtLink to="/about" class="flex p-2">
+             <img src="@/assets/images/Iconly-Bold-Activity.svg" class="img" height="15px" width="15px"/>
+          </NuxtLink>
+            <NuxtLink to="/" class="flex p-2">
+             <img src="@/assets/images/Iconly-Bold-Calendar.svg" class="img" height="15px" width="15px"/>
+          </NuxtLink>
+            <NuxtLink to="/" class=" flex p-2">
+             <img src="@/assets/images/Iconly-Bold-Info Square.svg" class="img" height="15px" width="15px"/>
+          </NuxtLink>
+  </div>
+</template>
+
+<style>
+   .bottombar{
+    display: none;
+  }
+    @media(max-width: 1024px){
+    .bottombar{
+      display: flex;
+      width: 100%;
+      height: 8vh;
+    }
+  }
+</style>
---
+++ components/Togglebar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="fixed top-0 togglebar p-3 overflow-auto" :class="`${active ? 'show' : ''}`">
+     <div class="flex flex-row justify-between mt-5">
+       <div class="inner flex flex-col">
+           <h3 class="greeting text-lg"><b>Hi Dev, Good Evening!</b></h3>
+           <p class="learn text-sm">Learn Web3,the Fun way</p>
+       </div>
+       <img class="close" src="@/assets/images/Iconly-Light-Outline-Close Square.svg" alt="close" height="25px" weight="25px" @click="close"/>
+     </div>
+       <div class="xp_info flex flex-col items-center mt-12">
+              <div class="img_border flex justify-center items-center">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXbpKeeGDLuC0SW1RvHziiFoQjVKIZo3EoOA&usqp=CAU" alt="profile" class="pr_img" />
+              </div>
+              <h4 class="name"><b>Novice</b></h4>
+              <p class="xp">1400/2000 XPs</p>
+          </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    props : {
+      toggle : {
+        type : Function
+      },
+      active: {
+        type : Boolean
+      }
+    },
+    methods:{
+       close(){
+         this.$emit('toggle');
+       }
+    }
+  }
+</script>
+
+<style>
+  .togglebar{
+      height: 100%;
+      width: 80%;
+      background-color: #3d3d3d;
+      z-index: 10;
+      position: fixed;
+      left: -80%;
+      transition: 0.5s ease-in-out;
+  }
+  .greeting,.learn{
+    color: white;
+  }
+  .close{
+    filter: invert(1);
+  }
+  .show{
+    left: 0%;
+  }
+</style>
