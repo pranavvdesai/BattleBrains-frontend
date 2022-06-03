fe16e7b953be3002e4ba5cdac9bdf941e8c23248
pranavvdesai <pranavdesai.psd@gmail.com>
2022-06-04 01:08:09 +0530

    feat: switch

---
+++ components/swit.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="w-14 h-6 flex items-center bg-gray-300 rounded-full p-1 duration-300 cursor-pointer bg-light-500"
+    :aria-checked="value.toString()"
+    @click="toggle"
+  >
+    <div
+      class="bg-blue-100 w-4 h-4 rounded-full shadow-md transform duration-300"
+      :class="{ 'translate-x-8': value }"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    toggle() {
+        this.$emit('input', !this.value)
+    }
+  }
+};
+</script>
