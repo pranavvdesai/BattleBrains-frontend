2e9c9ccc46d0a9e8b10db590ff9d74ef364ce7d9
pranavvdesai <pranavdesai.psd@gmail.com>
2022-06-04 01:46:06 +0530

    fix: right sidebar profile

--- components/peersidebar.vue
+++ components/peersidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="[ position ]"
-    class=" h-screen fixed top-0 lg:right-0 overflow-y-auto flex-row flex-nowrap overflow-hidden shadow-3xl bg-dark-300 lg:w-[20%] md:w-1/4 w-3/4 z-10 py-4 px-6 transition-all duration-300"
+    class=" h-screen fixed top-0 lg:right-0 overflow-y-auto flex-row flex-nowrap overflow-hidden shadow-3xl bg-dark-300 lg:w-[20%] w-3/4 z-10 py-4 px-6 transition-all duration-300"
   >
     <div class="flex justify-between items-center lg:justify-center">
       <h1 class="text-center my-6 text-light-300 text-xl font-mont">Your Peers</h1>
@@ -12,8 +12,8 @@
         class="w-3 h-3 mt-1 lg:hidden"
       >
     </div>
-    <div class="flex justify-between mx-5 mt-2 rounded-lg shadow-lg max-w-sm p-1 px-2 items-center">
-      <div class="">
+    <div class="flex lg:flex-col xl:flex-row mt-2 rounded-lg shadow-lg p-1 px-2 items-center">
+      <div class="mr-2">
         <img
           class=""
           src="@/assets/images/peerimg1.svg"
@@ -21,12 +21,14 @@
         />
 
       </div>
-      <div class="p-2">
+      <div class="p-2 text-center">
         <!-- <h5 class="text-gray-900 font-medium mb-2">Card title</h5> -->
         <span class="text-white">Pranav Desai</span>
           <p>@pranavvdesai</p>
       </div>
     </div>
+
+    
   </div>
 </template>
 
