3e897a51c17a403dd4c5558a13500a69c7e73717
HarshalHDave <71221177+HarshalHDave@users.noreply.github.com>
2022-06-07 00:27:57 +0530

    refactor: ui changes responsive and dynamic

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -7,163 +7,107 @@
 
       <div class="flex flex-col">
         <!-- middle -->
-        <div class=" lg:mx-[20%] bg-dark-100 h-screen ">
+        <div class="lg:mx-[20%] bg-dark-100 h-screen">
 
           <!-- mobilenav -->
-          <div class="lg:hidden mx-[5%]">
-            <div class="flex justify-between py-6 ">
-              <div class="flex items-center ">
-                <LeftArrowIcon
-                  class="mr-2"
-                  :size="16"
-                  :color="'#fff'"
-                />
-                <p class=" text-base font-bold text-white font-mont">BattleBrainz</p>
+          <div class="lg:hidden mx-[5%] flex justify-between py-6">
+            <div class="flex items-center">
+              <LeftArrowIcon class="mr-2" :size="16" :color="'#fff'" />
+              <p class=" text-base font-bold text-white font-mont">BattleBrainz</p>
+            </div>
+            <div class="flex items-center">
+              <div @click="toggleModal">
+                <SettingIcon class="mr-4" :size="16" :color="'#B8B8CC'" />
               </div>
-              <div class="flex items-center">
-                <div @click="toggleModal">
-                  <SettingIcon
-                    class="mr-4"
-                    :size="20"
-                    :color="'#B8B8CC'"
-                  />
-                </div>
-                <div @click="toggleSidebar">
-                  <UserIcon
-                    :size="20"
-                    :color="'#B8B8CC'"
-                  />
-                </div>
+              <div @click="toggleSidebar">
+                <UserIcon :size="16" :color="'#B8B8CC'" />
               </div>
             </div>
           </div>
 
           <!-- banner -->
-          <div class=" pt-4 mx-[5%]">
-            <img
-              src="@/assets/images/profile1.svg"
-              class="w-full"
-            />
-            <!-- dp -->
-            <!-- <img
-                  src="@/assets/images/dp3.jpg"
-                  class=" w-1/10 rounded-full transform sm:-translate-y-20 sm:ml-[10%] -translate-y-16 ml-[8%] border-dark-100 border-8"
-                /> -->
-
-            <!-- <div class=" w-fit absolute  top-[30%] right-[27%] bg-dark-300 p-2 h-fit rounded">
-                  <img src="@/assets/images/edit.svg" />
-                </div> -->
-          </div>
-
-          <!-- dptrial -->
-          <div class="flex justify-around  ">
-            <div class="  transform -translate-y-20 -translate-x-20 ">
-              <img
-                src="@/assets/images/dp3.jpg"
-                class=" lg:w-56 lg:h-56 md:w-48 md:h-48 sm:w-40 sm:h-40 w-32 h-32 rounded-full transform sm:-translate-y-5 sm:ml-[10%] translate-y-1 ml-[30%] border-dark-100 border-8"
-              />
+          <div class="relative lg:pt-6 pt-2 lg:mx-10 md:mx-10 sm:mx-5 mx-5 mb-[10%]">
+            <img src="@/assets/images/profile1.svg" class="w-full" />
+            <div class="absolute bg-gray-50 flex-row flex justify-between top-[45%] w-full px-10">
+              <img src="@/assets/images/dp3.png"
+                class="rounded-full w-[25%] border-dark-100 lg:border-8 md:border-6 border-4" />
+              <nuxt-link to="/profile/edit">
+                <div class="bg-dark-300 p-2 rounded-lg mt-4 absolute bottom-0 right-10">
+                  <img src="@/assets/images/edit.svg" class="lg:w-5 lg:h-5 md:w-4 md:h-4 sm:w-4 sm:h-4 w-4 h-4" />
+                </div>
+              </nuxt-link>
             </div>
-            <nuxt-link to="/profile/edit">
-              <div class=" mt-4 bg-dark-300 p-2 h-fit rounded">
-                <img src="@/assets/images/edit.svg" />
-              </div>
-            </nuxt-link>
           </div>
+
 
           <!-- formsection -->
-          <div class="mx-[12%] -mt-[4%]">
-            <div class="flex items-center pb-3">
-              <h1 class="font-mont text-3xl text-white mr-3">Pranav</h1>
-              <p class=" font-josefin text-blue-100">pranav@gmail.com</p>
+          <div class="lg:px-10 px-5">
+            <div class="flex items-center pb-1">
+              <h1 class="font-mont text-2xl text-white mr-3 font-extrabold">Pranav</h1>
+              <p class=" font-josefin text-blue-100 lg:text-lg md:text-lg sm:text-base text-base">
+                pranav@gmail.com</p>
             </div>
             <div>
-              <p class=" text-dark-600">21 | Computational Graphics | Deep Learning | Design and UX | ΨΦ | Building next gen web3 edtech @battlebrains_</p>
+              <p class="text-dark-600 lg:text-base md:text-base sm:text-sm text-sm font-josefin">21 | Computational
+                Graphics |
+                Deep
+                Learning | Design and UX
+                | ΨΦ | Building next
+                gen web3 edtech @battlebrains_</p>
             </div>
             <div class="flex items-stretch mt-5">
-              <div class="bg-dark-300 p-2 mr-2 rounded-lg">
-                <img
-                  src="@/assets/images/github.svg"
-                  class="w-6 h-6"
-                />
-              </div>
-              <div class="bg-dark-300 p-2 mr-2 rounded-lg">
-                <img
-                  src="@/assets/images/twitter.svg"
-                  class="w-6 h-6"
-                />
-              </div>
-              <div class="bg-dark-300 p-2 mr-2 rounded-lg">
-                <img
-                  src="@/assets/images/linkedin.svg"
-                  class="w-6 h-6"
-                />
-              </div>
-              <div class="bg-dark-300 p-2 mr-2 rounded-lg">
-                <img
-                  src="@/assets/images/discord.svg"
-                  class="w-6 h-6"
-                />
-              </div>
-              <div class="bg-dark-300 p-2 mr-2 rounded-lg">
-                <img
-                  src="@/assets/images/behance.svg"
-                  class="w-6 h-6"
-                />
-              </div>
-              <div class="bg-dark-300 p-2">
-                <img
-                  src="@/assets/images/medium.svg"
-                  class="w-6 h-6"
-                />
-              </div>
+              <a href="https://battlebrains.xyz" target="_blank" class="bg-dark-300 p-2 mr-2 rounded-lg">
+                <img src="@/assets/images/github.svg" class="w-5 h-5" />
+              </a>
+              <a href="https://battlebrains.xyz" target="_blank" class="bg-dark-300 p-2 mr-2 rounded-lg">
+                <img src="@/assets/images/twitter.svg" class="w-5 h-5" />
+              </a>
+              <a href="https://battlebrains.xyz" target="_blank" class="bg-dark-300 p-2 mr-2 rounded-lg">
+                <img src="@/assets/images/linkedin.svg" class="w-5 h-5" />
+              </a>
+              <a href="https://battlebrains.xyz" target="_blank" class="bg-dark-300 p-2 mr-2 rounded-lg">
+                <img src="@/assets/images/discord.svg" class="w-5 h-5" />
+              </a>
+              <a href="https://battlebrains.xyz" target="_blank" class="bg-dark-300 p-2 mr-2 rounded-lg">
+                <img src="@/assets/images/behance.svg" class="w-5 h-5" />
+              </a>
+              <a href="https://battlebrains.xyz" target="_blank" class="bg-dark-300 p-2 rounded-lg">
+                <img src="@/assets/images/medium.svg" class="w-5 h-5" />
+              </a>
             </div>
-            <div class="grid grid-cols-3 text-dark-500">
+            <div class="grid grid-cols-3 text-dark-500 font-josefin">
+
               <div class="col-span-3 py-2 pt-6">
                 Your referral link
               </div>
-              <div class="col-span-3">
-                <input
-                  type="text"
-                  class="bg-dark-300 p-2 rounded w-full"
-                  value="https://www.battlebrains.com/invite/pranav"
-                />
+              <div class="lg:col-span-2 col-span-3 bg-dark-300 p-2 rounded-md w-full font-mont text-sm text-blue-100">
+                https://battlebrains.xyz/register?ref=pranav
               </div>
+              <button
+                class="col-span-1 lg:block hidden bg-dark-300 p-2 rounded ml-4 font-josefin text-sm font-bold w-20">Copy</button>
+
               <div class="col-span-3 py-2">
                 Metamask
               </div>
-              <div class="lg:col-span-2 col-span-3">
-                <input
-                  type="text"
-                  class="bg-dark-300 p-2 rounded w-full"
-                  value="https://www.battlebrains.com/invite/pranav"
-                />
-              </div>
-              <div class="col-span-1 lg:block hidden">
-                <button class="bg-dark-300 p-2 rounded ml-4">Connect</button>
-              </div>
+              <input type="text" disabled
+                class="lg:col-span-2 col-span-3 bg-dark-300 p-2 rounded-md w-full font-mont text-sm"
+                value="https://www.battlebrains.com/invite/pranav" />
+              <button
+                class="col-span-1 lg:block hidden bg-dark-300 p-2 rounded ml-4 font-josefin text-sm font-bold w-20">Connect</button>
+
               <div class="col-span-3 py-2">
                 Email
               </div>
-              <div class="lg:col-span-2 col-span-3">
-                <input
-                  type="email"
-                  class="bg-dark-300 p-2 rounded w-full"
-                  value="https://www.battlebrains.com/invite/pranav"
-                />
-              </div>
-              <div class="col-span-1 lg:block hidden">
-                <button class="bg-dark-300 p-2 rounded ml-4">Verified</button>
-              </div>
+              <input type="email" class="lg:col-span-2 col-span-3 bg-dark-300 p-2 rounded w-full font-mont text-sm"
+                value="https://www.battlebrains.com/invite/pranav" />
+              <button
+                class="col-span-1 lg:block hidden bg-dark-300 p-2 rounded ml-4 font-josefin text-sm font-bold w-20">Verified</button>
+
               <div class="col-span-3 py-2">
                 Age group
               </div>
-              <div class="lg:col-span-2 col-span-3 pb-6">
-                <input
-                  type="email"
-                  class="bg-dark-300 p-2 rounded w-full"
-                  value="https://www.battlebrains.com/invite/pranav"
-                />
-              </div>
+              <input type="email" class="lg:col-span-2 col-span-3 mb-6 bg-dark-300 p-2 rounded w-full font-mont text-sm"
+                value="https://www.battlebrains.com/invite/pranav" />
             </div>
           </div>
         </div>
@@ -171,15 +115,9 @@
       </div>
 
       <!-- rightsidebar -->
-      <Peersidebar
-        @toggle="toggleSidebar"
-        :class="position"
-      />
+      <Peersidebar @toggle="toggleSidebar" :class="position" />
       <!-- bottomnav -->
-      <div
-        :class="{hidden : hidden}"
-        class="  fixed bg-dark-300 shadow-lg bottom-0 left-0 w-full lg:hidden"
-      >
+      <div :class="{ hidden: hidden }" class="  fixed bg-dark-300 shadow-lg bottom-0 left-0 w-full lg:hidden">
         <Modalprofile />
       </div>
 
