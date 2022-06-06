3388567e29c2ddacde9c41a5e1923f84abfd24c4
krishvadhani19 <krish.vadhani@somaiya.edu>
2022-06-06 18:57:24 +0530

    blog page done

--- components/leftsidebar.vue
+++ components/leftsidebar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="h-screen fixed top-0 hidden lg:block lg:left-0 overflow-y-auto flex-row flex-nowrap overflow-hidden shadow-3xl bg-dark-300 lg:w-[20%] w-1/4 z-10 py-4 px-4 transition-all duration-300">
+  <div
+    class="h-screen fixed top-0 hidden lg:block lg:left-0 overflow-y-auto flex-row flex-nowrap overflow-hidden shadow-3xl bg-dark-300 lg:w-[20%] w-1/4 z-10 py-4 px-4 transition-all duration-300"
+  >
     <div class="">
       <div class="flex mt-4 align-middle">
         <leftArrowIcon :color="'#9898B3'" :size="14" />
@@ -11,10 +13,15 @@
         Why Web3?
       </h1>
       <div class="flex flex-col lg:flex-row mt-4">
-        <h2 class="text-blue-300 mr-2 text-sm font-josefin pb-1 lg:pb-0">INTRODUCTION</h2>
+        <h2 class="text-blue-300 mr-2 text-sm font-josefin pb-1 lg:pb-0">
+          INTRODUCTION
+        </h2>
         <h2 class="text-pink text-sm font-josefin">BLOCKCHAIN</h2>
-      </div>y
-      <p class="mt-8 text-lg text-light-300 font-josefin">A blockchain is a digitally distributed, decentralized, public ledger that exists across a network...</p>
+      </div>
+      <p class="mt-8 text-lg text-light-300 font-josefin">
+        A blockchain is a digitally distributed, decentralized, public ledger
+        that exists across a network...
+      </p>
       <div class="flex items-center">
         <h1 class="text-white mt-6 mb-6 mr-3 font-josefin">2/4 videos</h1>
         <div class="w-1/2 bg-white rounded-lg">
@@ -23,9 +30,7 @@
       </div>
     </div>
     <div class="absolute bottom-0 mb-10 w-4/5 self-center">
-      <div
-        class="flex border border-dark-500 rounded-md justify-between p-4"
-      >
+      <div class="flex border border-dark-500 rounded-md justify-between p-4">
         <p class="text-dark-600">Test Yourself</p>
         <PaperIcon :size="16" :color="'#9898B3'" />
       </div>
@@ -37,7 +42,8 @@
 import leftArrowIcon from "./icons/leftArrowIcon";
 import PaperIcon from "./icons/paperIcon";
 
-export default {name: "leftsidebar",
+export default {
+  name: "leftsidebar",
   components: {
     leftArrowIcon,
     PaperIcon,
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,56 +1,90 @@
 <template>
-  <section class="bg-dark-200 h-screen">
-    <Leftsidebar />
-    <div class="flex justify-end">
-      <div class="header flex flex-col mr-10 mt-3 w-3/4 lg:w-[80%]">
-        <div class="text-2xl md:text-4xl lg:text-5xl text-white font-bold">
-          What Is Web 3.0 & Why It Matters
-        </div>
-        <div class="flex mt-4 font-medium">
-          <div class="mr-4 text-[#01E3FF]">INTRODUCTION</div>
-          <div class="mr-4 text-[#FF01EC]">BLOCKCHAIN</div>
-        </div>
-        <div class="flex mt-2 text-[#94A3B8] font-medium text-sm">
-          <div class="mr-3">Dec 31, 2019</div>
-          <div class="mr-3 font-black">.</div>
-          <div class="mr-3">7mins</div>
-        </div>
-        <div class="text-white mt-6">
-          <div class="flex justify-center">
-            <img
-              class="w-3/5 h-1/3"
-              src="https://imgs.search.brave.com/U4XbX-OnZ650_zYGCFeKLc_-LDbHSGx2GqWxvk6BB5w/rs:fit:772:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5X/T0xueVFReWtIUnda/YVdxMkZnQ19BSGFF/aiZwaWQ9QXBp"
-              alt=""
-            />
+  <body class="bg-dark-100">
+    <section class="">
+      <Leftsidebar />
+      <div class="flex lg:justify-end">
+        <div
+          class="header flex flex-col mt-3 w-[87%] lg:w-[80%] lg:ml-0 ml-8 sm:ml-10 md:ml-14"
+        >
+          <div class="lg:hidden">
+            <div class="flex justify-between py-6">
+              <div class="flex items-center">
+                <LeftArrowIcon class="mr-2" :size="16" :color="'#fff'" />
+                <p class="text-base font-bold text-white font-mont">
+                  Why Web3?
+                </p>
+              </div>
+              <div class="flex items-center">
+                <div @click="toggleModal">
+                  <InfoIcon class="mr-4" :size="20" :color="'#B8B8CC'" />
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="text-white mt-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-            dolores laboriosam accusamus vel, possimus quaerat voluptatem aut
-            architecto omnis quasi laborum, quod atque? Eos corrupti facilis
-            porro exercitationem ex! Possimus labore fugiat nobis unde,
-            veritatis ratione eos expedita qui? Nisi nemo nesciunt pariatur
-            molestiae eaque temporibus repellat quae laboriosam, est possimus
-            esse delectus voluptas voluptate quisquam ipsum reiciendis cum
-            cupiditate voluptatem consequuntur facilis iste aspernatur sunt
-            sequi placeat! Porro consectetur expedita nostrum ipsa facere
-            dolores eaque enim natus, quam maiores voluptas ratione provident.
-            Sed sint expedita ratione repellendus facere repellat eveniet rerum
-            perferendis, autem omnis quasi placeat illo quibusdam.
+
+          <div
+            class="text-4xl md:text-5xl lg:text-6xl text-white font-bold lg:mx-10"
+          >
+            What Is Web 3.0 & Why It Matters?
+          </div>
+          <div class="flex mt-4 font-medium lg:mx-10">
+            <div class="mr-4 text-[#01E3FF]">INTRODUCTION</div>
+            <div class="mr-4 text-[#FF01EC]">BLOCKCHAIN</div>
+          </div>
+          <div class="flex mt-2 text-[#94A3B8] font-medium text-sm lg:mx-10">
+            <div class="mr-3">Dec 31, 2019</div>
+            <div class="mr-3 font-black">.</div>
+            <div class="mr-3">7mins</div>
+          </div>
+          <div class="text-white mt-6">
+            <div class="flex justify-center">
+              <img
+                class="w-[85%] h-[45%] lg:w-[50%] lg:h-[30%]"
+                src="https://imgs.search.brave.com/U4XbX-OnZ650_zYGCFeKLc_-LDbHSGx2GqWxvk6BB5w/rs:fit:772:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5X/T0xueVFReWtIUnda/YVdxMkZnQ19BSGFF/aiZwaWQ9QXBp"
+                alt=""
+              />
+            </div>
+            <div class="text-white mx-2 lg:mx-10 mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              dolores laboriosam accusamus vel, possimus quaerat voluptatem aut
+              architecto omnis quasi laborum, quod atque? Eos corrupti facilis
+              porro exercitationem ex! Possimus labore fugiat nobis unde,
+              veritatis ratione eos expedita qui? Nisi nemo nesciunt pariatur
+              molestiae eaque temporibus repellat quae laboriosam, est possimus
+              esse delectus voluptas voluptate quisquam ipsum reiciendis cum
+              cupiditate voluptatem consequuntur facilis iste aspernatur sunt
+              sequi placeat! Porro consectetur expedita nostrum ipsa facere
+              dolores eaque enim natus, quam maiores voluptas ratione provident.
+              Sed sint expedita ratione repellendus facere repellat eveniet
+              rerum perferendis, autem omnis quasi placeat illo quibusdam.
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+
+      <!-- bottomnav -->
+      <div
+        :class="{ hidden: hidden }"
+        class="fixed bg-dark-300 shadow-lg bottom-0 left-0 w-full lg:hidden"
+      >
+        <Modal />
+      </div>
+    </section>
+  </body>
 </template>
 
 <script>
 import { marked } from "marked";
 import Leftsidebar from "../components/leftsidebar.vue";
+import LeftArrowIcon from "../components/icons/leftArrowIcon.vue";
+import InfoIcon from "../components/icons/infoIcon.vue";
+import Modal from "../components/modal.vue";
 
 export default {
   data() {
     return {
       markdown: "",
+      hidden: true,
     };
   },
   computed: {
@@ -68,6 +102,11 @@ export default {
       return marked(this.markdown);
     },
   },
-  components: { Leftsidebar },
+  methods: {
+    toggleModal() {
+      this.hidden = !this.hidden;
+    },
+  },
+  components: { Leftsidebar, LeftArrowIcon, InfoIcon, Modal },
 };
 </script>
