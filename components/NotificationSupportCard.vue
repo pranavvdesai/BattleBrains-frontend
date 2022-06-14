221e0c9b449338329441641c13d54bb42ee089b3
MaheraFurniturewala <maherafurniturewala1111@gmail.com>
2022-06-14 22:54:05 +0530

    add notification page

---
+++ components/NotificationSupportCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex justify-center mb-5">
+    <div
+      class="
+        rounded-xl
+        hover:shadow-[0_0_10px_5px_rgba(0,0,0,0.3)]
+        bg-dark-300
+        max-w-sm
+        p-2
+      "
+    >
+      <div>
+        <p class="text-sm text-dark-500 mb-1">request #{{ id }}</p>
+        <h2 class="text-white text-lg font-bold font-mont mb-1">{{ title }}</h2>
+        <p class="text-dark-600 text-sm my-1 font-josefin mb-4">
+          {{ desc }}
+        </p>
+        <button
+          class="w-1/4 rounded-lg text-white"
+          v-bind:class="{
+            'bg-blue-300': open,
+            'bg-dark-500': !open,
+          }"
+        >
+          {{ open ? "open" : "closed" }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotificationSupportCard",
+  props: {
+    id: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    desc: {
+      type: String,
+    },
+    open: {
+      type: Boolean,
+    },
+  },
+};
+</script>
---
+++ components/Notifications.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="flex justify-between flex:1 ml-4 mb-12">
+    <img
+      src="@/assets/images/notifications.png"
+      class="h-16 w-16"
+      alt="notifications"
+    />
+    <div class="relative w-full ml-8">
+      <div class="flex-col absolute top-0">
+        <span class="text-lg font-mont text-dark-700 tracking-wide">
+          <b>{{ title }}</b>
+        </span>
+        <span class="text-sm font-josefin text-dark-500 xs:mx-3 lg:mx-6">{{
+          time
+        }}</span>
+      </div>
+      <p class="text-sm font-josefin text-dark-500 absolute bottom-0">
+        {{ description }}
+      </p>
+    </div>
+    <button
+      class="
+        hidden
+        lg:block
+        my-4
+        w-1/6
+        h-8
+        text-white
+        border-solid
+        bg-dark-200
+        rounded-lg
+        text-sm
+        border-blue-100 border-2
+        font-josefin
+      "
+    >
+      visit
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Notifications",
+  props: {
+    title: {
+      type: String,
+    },
+    time: {
+      type: String,
+    },
+    date: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+  },
+};
+</script>
